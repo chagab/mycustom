4ca8b404f41a13f266093c7791569489fa16e25c
chagab <gabriel.chatelain@googlemail.com>
2017-04-30 11:52:14 +0200

    Add files via upload

---
+++ collection_ephemere/models.py
@@ -0,0 +1,66 @@
+#-*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.db import models
+
+
+CONTOUR_CHOICES = (
+	('dotted','dotted'),
+	('dashed','dashed'),
+	('solid','solid'),
+	('double','double'),
+	('groove','groove'),
+	('ridge','ridge'),
+	('inset','inset'),
+	('outset','outset'),
+	('none','none'),
+	('hidden','hidden'),
+)
+
+COLONNE_CHOICES = (
+	('colonne_1','colonne_1'),
+	('colonne_2','colonne_2'),
+	('colonne_3','colonne_3'),
+)
+
+class ongletCollectionEphemere(models.Model):
+	nom           = models.CharField(max_length=140)
+	date          = models.DateField()
+	image_titre   = models.ImageField(upload_to="collectionEphemere/image_titre/")
+	couleur_titre = models.CharField(max_length=140, default='white')
+	description   = models.TextField()
+	confirm       = models.BooleanField()
+
+
+	class Meta:
+		verbose_name = "Présentation de l'onglet Achetez"
+		verbose_name_plural = "Présentation de l'onglet Achetez"
+
+	def __str__(self):
+		return self.nom
+
+	def __unicode__(self):
+		return self.nom
+
+class mosaique(models.Model):
+	image            = models.ImageField(upload_to="collection_ephemere/mosaique")
+	titre            = models.CharField(max_length=140)
+	text_description = models.TextField()
+	colonne          = models.CharField(max_length=15,choices=COLONNE_CHOICES,default='none')
+	confirm          = models.BooleanField()
+	video 	         = models.BooleanField()
+	video_mp4        = models.FileField(upload_to="collection_ephemere/video/")
+	video_webm       = models.FileField(upload_to="collection_ephemere/video/", blank=True)
+
+
+class collectionEphemere(models.Model):
+	image                 = models.ImageField(upload_to="collection_ephemere/image/")
+	text_description      = models.TextField()
+	couleur_text          = models.CharField(max_length=7,default='#111111')
+	couleur_fond          = models.CharField(max_length=7,default='#FFFFFF')
+	contour_arrondi       = models.PositiveSmallIntegerField(default=0)
+	contour_arrondi_image = models.PositiveSmallIntegerField(default=0)
+	type_contour          = models.CharField(max_length=15,choices=CONTOUR_CHOICES,default='none')
+	couleur_contour       = models.CharField(max_length=7)
+	epaisseur_contour     = models.PositiveSmallIntegerField(default=0)
+	pub               	  = models.BooleanField()
+
