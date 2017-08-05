33ad729e44caa0fcebfa8e9d8b8561a0aaea3563
chagab <gabriel.chatelain@googlemail.com>
2017-08-05 11:39:40 +0200

    css min
    minifying css with grunt

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,23 @@
-module.export = function(grunt) {
-	gunt.initConfig({
+module.exports = function(grunt) {
+	const pathCss = "/Users/gabrielchatelain/Desktop/mycustom/static/mySelf/css/";
+	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		cssmin: {
+			my_target: {
+				files: [{
+					expand: true,
+					cwd: pathCss,
+					src: ['*.css', '!*.min.css'],
+					dest: pathCss,
+					ext: ".min.css"
+				}]
+				/*combine: {
+					files: {
+						"static/allCss.css": [pathCss + "/*.min.css"],
+					}
+				}*/
+			}
+		}
 	});
-}
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+};
